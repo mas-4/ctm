b86524e4bdd22722c3c7b27e32ddef0973c81b28
mas <m@stdwtr.io>
2021-08-13 20:20:42 -0400

    Fix something

--- linlib/comb.py
+++ linlib/comb.py
@@ -1,6 +1,6 @@
 from typing import Union
 from linlib.vec import Vec
 
-def lin_comb(vlist: list[Vec], clist: list[Union[float,int]]) -> Union[int,float]:
+def lin_comb(vlist: list[Vec], clist: list[Union[float,int]]) -> Union[Vec,int]:
     assert len(vlist) == len(clist)
     return sum([coeff*v for (coeff,v) in zip(clist,vlist)])
